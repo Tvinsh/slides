bc7f7a008c9c057fff546719147daaca4495f7c1
weixian <weixian@pinduoduo.com>
2020-05-29 16:17:14 +0800

    feat: update

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  publicPath: "/",
+  productionSourceMap: false,
+  outputDir: "docs",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src")
+      }
+    },
+    plugins: []
+  },
+  chainWebpack() {},
+  devServer: {
+    host: "127.0.0.1",
+    port: 80,
+    allowedHosts: ["dev.mms.com"]
+  }
+};
